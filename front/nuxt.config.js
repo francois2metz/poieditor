c300f57f1757e4179595201656538fadd7771c4e
François de Metz <francois@2metz.fr>
2020-10-26 18:14:57 +0100

    Remove the favicon.

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -12,9 +12,6 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
